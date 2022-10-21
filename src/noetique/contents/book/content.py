ed7706c19237a61e07ac212709c1dcaa4079a53f
Sébastien Verbois <sebastien.verbois@imio.be>
2022-10-21 19:25:35 +0200

    Black

--- src/noetique/contents/book/content.py
+++ src/noetique/contents/book/content.py
@@ -1,7 +1,8 @@
 from plone.app.textfield import RichText
 from plone.autoform import directives
 from plone.dexterity.content import Item
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.field import NamedBlobFile
+from plone.namedfile.field import NamedBlobImage
 from plone.supermodel import model
 from zope import schema
 from zope.interface import implementer
@@ -11,83 +12,81 @@ class IBook(model.Schema):
     """Book schema"""
 
     # fields
-    title = schema.TextLine(
-        title=u"Titre du livre",
-        required=True
-    )
+    title = schema.TextLine(title="Titre du livre", required=True)
     author = schema.TextLine(
-        title=u"Auteur(s)",
-        default=u"Marc Halévy",
+        title="Auteur(s)",
+        default="Marc Halévy",
         required=False,
     )
     description = schema.Text(
-        title=u"Présentation du livre",
+        title="Présentation du livre",
         required=True,
     )
     audience = schema.TextLine(
-        title=u"Public",
+        title="Public",
         required=False,
     )
     cover = NamedBlobImage(
-        title=u"Photo de couverture",
+        title="Photo de couverture",
         required=False,
     )
     summary = RichText(
-        title=u"Résumé détaillé du livre",
+        title="Résumé détaillé du livre",
         required=False,
     )
     foreword = RichText(
-        title=u"Préface du livre",
+        title="Préface du livre",
         required=False,
     )
     book = NamedBlobFile(
-        title=u"Version gratuite du livre",
+        title="Version gratuite du livre",
         required=False,
     )
     publisher = schema.TextLine(
-        title=u"Editeur",
+        title="Editeur",
         required=False,
     )
     collection = schema.TextLine(
-        title=u"Collection",
+        title="Collection",
         required=False,
     )
     kind = schema.TextLine(
-        title=u"Genre",
+        title="Genre",
         required=False,
     )
     langue = schema.TextLine(
-        title=u"Langue",
+        title="Langue",
         required=False,
     )
     pages = schema.TextLine(
-        title=u"Nombre de pages",
+        title="Nombre de pages",
         required=False,
     )
     format = schema.TextLine(
-        title=u"Format",
+        title="Format",
         required=False,
     )
     year = schema.TextLine(
-        title=u"Année de publication",
+        title="Année de publication",
         required=False,
     )
     isbn = schema.TextLine(
-        title=u"ISBN",
+        title="ISBN",
         required=False,
     )
 
     # widgets
-    directives.widget('description', rows=5)
-    directives.widget('summary', rows=10)
-    directives.widget('foreword', rows=10)
+    directives.widget("description", rows=5)
+    directives.widget("summary", rows=10)
+    directives.widget("foreword", rows=10)
 
     # fieldsets
     model.fieldset(
-        'metadata',
-        label=u"Metadata",
-        fields=['isbn', 'publisher', 'year', 'format', 'pages', 'collection', 'kind', 'langue']
+        "metadata",
+        label="Metadata",
+        fields=["isbn", "publisher", "year", "format", "pages", "collection", "kind", "langue"],
     )
+
 
 @implementer(IBook)
 class Book(Item):
--- src/noetique/views/home.py
+++ src/noetique/views/home.py
@@ -6,46 +6,44 @@ from Products.Five import BrowserView
 
 
 class HomeView(BrowserView):
-
     @property
     def portal_path(self):
         portal = api.portal.get()
-        return '/'.join(portal.getPhysicalPath())
+        return "/".join(portal.getPhysicalPath())
 
     @property
     def last_articles(self):
         brains = api.content.find(
-            portal_type=('Document', 'File'),
-            path=self.portal_path + '/billets',
-            review_state='published',
-            sort_on='effective',
-            sort_order='reverse',
-            sort_limit=3
+            portal_type=("Document", "File"),
+            path=self.portal_path + "/billets",
+            review_state="published",
+            sort_on="effective",
+            sort_order="reverse",
+            sort_limit=3,
         )
         if len(brains) < 1:
             return None
 
-        articles =[]
+        articles = []
         for b in brains[:3]:
             article = {
-                'title': b.Title,
-                'description': b.Description,
-                'url': b.getURL()+'/view',
-                'effective': b.effective.ISO(),
+                "title": b.Title,
+                "description": b.Description,
+                "url": b.getURL() + "/view",
+                "effective": b.effective.ISO(),
             }
             articles.append(article)
         return articles
 
-
     @property
     def last_news(self):
         brains = api.content.find(
-            portal_type='News Item',
-            path=self.portal_path + '/actualites',
-            review_state='published',
-            sort_on='effective',
-            sort_order='reverse',
-            sort_limit=3
+            portal_type="News Item",
+            path=self.portal_path + "/actualites",
+            review_state="published",
+            sort_on="effective",
+            sort_order="reverse",
+            sort_limit=3,
         )
         if len(brains) < 1:
             return None
@@ -53,53 +51,58 @@ class HomeView(BrowserView):
         brain = brains[0]
         obj = brain.getObject()
         return {
-            'title': brain.Title,
-            'description': brain.Description,
-            'url': brain.getURL(),
-            'effective': brain.effective,
-            'has_image': True if obj.image else False,
+            "title": brain.Title,
+            "description": brain.Description,
+            "url": brain.getURL(),
+            "effective": brain.effective,
+            "has_image": True if obj.image else False,
         }
 
     @property
     def last_thought(self):
         brains = api.content.find(
-            portal_type='Document',
-            path=self.portal_path + '/journal',
-            review_state='published',
-            sort_on='effective',
-            sort_order='reverse',
-            sort_limit=3
+            portal_type="Document",
+            path=self.portal_path + "/journal",
+            review_state="published",
+            sort_on="effective",
+            sort_order="reverse",
+            sort_limit=3,
         )
         if len(brains) < 1:
             return None
 
         brain = brains[0]
         return {
-            'title': brain.Title,
-            'description': brain.Description,
-            'url': brain.getURL(),
-            'effective': brain.effective,
+            "title": brain.Title,
+            "description": brain.Description,
+            "url": brain.getURL(),
+            "effective": brain.effective,
         }
 
     @property
     def next_events(self):
         brains = api.content.find(
-            portal_type='Event',
-            path=self.portal_path + '/agenda',
-            review_state='published',
-            end = {'query':[DateTime(),], 'range':'min'},
-            sort_on='start',
-            sort_order='ascending',
+            portal_type="Event",
+            path=self.portal_path + "/agenda",
+            review_state="published",
+            end={
+                "query": [
+                    DateTime(),
+                ],
+                "range": "min",
+            },
+            sort_on="start",
+            sort_order="ascending",
         )
-        events =[]
+        events = []
         for b in brains[:3]:
             obj = b.getObject()
             event = {
-                'title': b.Title,
-                'description': b.Description,
-                'start': b.start,
-                'location': obj.location,
-                'url': b.getURL(),
+                "title": b.Title,
+                "description": b.Description,
+                "start": b.start,
+                "location": obj.location,
+                "url": b.getURL(),
             }
             events.append(event)
         return events
@@ -107,23 +110,23 @@ class HomeView(BrowserView):
     @property
     def last_books(self):
         brains = api.content.find(
-            portal_type='noetique.site.Book',
-            path=self.portal_path + '/livres',
-            review_state='published',
-            sort_on='effective',
-            sort_order='reverse',
-            sort_limit=3
+            portal_type="noetique.site.Book",
+            path=self.portal_path + "/livres",
+            review_state="published",
+            sort_on="effective",
+            sort_order="reverse",
+            sort_limit=3,
         )
-        books =[]
+        books = []
         for b in brains[:3]:
             obj = b.getObject()
             book = {
-                'title': obj.title,
-                'description': obj.description,
-                'author': obj.author,
-                'publisher': obj.publisher,
-                'year': obj.year,
-                'url': obj.absolute_url(),
+                "title": obj.title,
+                "description": obj.description,
+                "author": obj.author,
+                "publisher": obj.publisher,
+                "year": obj.year,
+                "url": obj.absolute_url(),
             }
             books.append(book)
         return books
@@ -131,20 +134,20 @@ class HomeView(BrowserView):
     @property
     def last_video(self):
         brains = api.content.find(
-            portal_type='Link',
-            path=self.portal_path + '/videos',
-            review_state='published',
-            sort_on='effective',
-            sort_order='reverse',
-            sort_limit=3
+            portal_type="Link",
+            path=self.portal_path + "/videos",
+            review_state="published",
+            sort_on="effective",
+            sort_order="reverse",
+            sort_limit=3,
         )
         if len(brains) < 1:
             return None
 
         link = brains[0].getObject()
         video = {
-            'title': link.title,
-            'description': link.description,
-            'video_id': link.remoteUrl.split('?v=')[1] if '?v=' in link.remoteUrl else ''
+            "title": link.title,
+            "description": link.description,
+            "video_id": link.remoteUrl.split("?v=")[1] if "?v=" in link.remoteUrl else "",
         }
         return video
